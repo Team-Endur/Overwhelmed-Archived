98184976307b42982cbd5a145acd628e57f1b51b
Jeremiah Wimbrough <j.wimbrough@gmail.com>
2023-06-22 08:02:40 -0400

    Added BlockRegistry with a test block as an example

--- common/src/main/java/overwhelmed/overwhelmed/Overwhelmed.java
+++ common/src/main/java/overwhelmed/overwhelmed/Overwhelmed.java
@@ -6,6 +6,8 @@ import dev.architectury.registry.registries.RegistrarManager;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
+import overwhelmed.overwhelmed.registry.BlockRegistry;
 import overwhelmed.overwhelmed.registry.CreativeTabRegistry;
 import overwhelmed.overwhelmed.registry.EntityRegistry;
 import overwhelmed.overwhelmed.registry.ItemRegistry;
@@ -19,15 +21,18 @@ public class Overwhelmed {
 	public static final Supplier<RegistrarManager> MANAGER = Suppliers.memoize(() -> RegistrarManager.get(MOD_ID));
 	public static Registrar<EntityType<?>> entityTypes;
 	public static Registrar<Item> items;
+	public static Registrar<Block> blocks;
 
 	public static void init() {
 
 		entityTypes = MANAGER.get().get(Registries.ENTITY_TYPE);
 		items = MANAGER.get().get(Registries.ITEM);
+		blocks = MANAGER.get().get(Registries.BLOCK);
 
 		CreativeTabRegistry.registerCreativeTab();
 		EntityRegistry.registerEntities();
 		ItemRegistry.registerItems();
+		BlockRegistry.registerBlocks();
 
 	}
 }
