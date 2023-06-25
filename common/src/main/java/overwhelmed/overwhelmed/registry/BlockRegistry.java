f8eaba9034db5d6eddc9b77a4255afd9862d638c
Jeremiah Wimbrough <j.wimbrough@gmail.com>
2023-06-25 09:44:26 -0400

    Created helper methods for registering blocks and items. This will help to reduce duplicate code and make it easier to read.

--- common/src/main/java/overwhelmed/overwhelmed/registry/BlockRegistry.java
+++ common/src/main/java/overwhelmed/overwhelmed/registry/BlockRegistry.java
@@ -2,19 +2,40 @@ package overwhelmed.overwhelmed.registry;
 
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import overwhelmed.overwhelmed.Overwhelmed;
 
 public class BlockRegistry {
     /*
-        public static RegistrySupplier<Block> testBlock;
+    public static RegistrySupplier<Block> testBlock;
      */
     public static void registerBlocks() {
         /*
-            testBlock = Overwhelmed.blocks.register(new ResourceLocation(Overwhelmed.MOD_ID, "test_block"), () ->
-                    new Block(Block.Properties.of().mapColor(MapColor.STONE).strength(1.5f, 6.0f).requiresCorrectToolForDrops()));
+        testBlock = registerBlock("test_block", BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(1.5f, 6.0f)
+                    .requiresCorrectToolForDrops());
          */
+
         Overwhelmed.BLOCKS.register();
+        /*
+        registerBlockItem("test_block", testBlock);
+         */
+
+
+    }
+
+    private static RegistrySupplier<Block> registerBlock(String name, Block.Properties properties) {
+        return Overwhelmed.BLOCKS.register(new ResourceLocation(Overwhelmed.MOD_ID, name), () ->
+                new Block(properties));
+    }
+
+    private static void registerBlockItem(String name, RegistrySupplier<Block> blockSupplier) {
+        Overwhelmed.ITEMS.register(new ResourceLocation(Overwhelmed.MOD_ID, name), () ->
+                new BlockItem(blockSupplier.get(), new Item.Properties().arch$tab(CreativeTabRegistry.overwhelmedTab)));
     }
 }
--- common/src/main/java/overwhelmed/overwhelmed/registry/ItemRegistry.java
+++ common/src/main/java/overwhelmed/overwhelmed/registry/ItemRegistry.java
@@ -15,52 +15,36 @@ public class ItemRegistry {
     public static RegistrySupplier<Item> snailShellItem;
     public static RegistrySupplier<SnailSpawnEggItem> snailSpawnEggItem;
     public static RegistrySupplier<Item> gooBallItem;
+
     /*
         public static RegistrySupplier<Item> goldBeadItem;
         public static RegistrySupplier<Item> iceCrystalShardItem;
         public static RegistrySupplier<Item> iceCubeItem;
         public static RegistrySupplier<Item> pebbleItem;
-        public static RegistrySupplier<BlockItem> testBlockItem;
-    */
+     */
+
     public static void registerItems() {
-        snailShellItem = Overwhelmed.ITEMS.register(new ResourceLocation(Overwhelmed.MOD_ID, "snail_shell"), () ->
-                new Item(new Item.Properties()
-                        .stacksTo(64)
-                        .rarity(Rarity.COMMON)
-                        .arch$tab(overwhelmedTab)));
-        gooBallItem = Overwhelmed.ITEMS.register(new ResourceLocation(Overwhelmed.MOD_ID, "goo_ball"), () ->
-                new Item(new Item.Properties()
-                        .stacksTo(64)
-                        .rarity(Rarity.COMMON)
-                        .arch$tab(overwhelmedTab)));
+        snailShellItem = registerItem("snail_shell", 64, Rarity.COMMON);
+        gooBallItem = registerItem("goo_ball", 64, Rarity.COMMON);
+
         snailSpawnEggItem = Overwhelmed.ITEMS.register(new ResourceLocation(Overwhelmed.MOD_ID, "snail_spawn_egg"), () ->
                 new SnailSpawnEggItem(new Item.Properties()
                         .arch$tab(overwhelmedTab)));
         /*
-            goldBeadItem = Overwhelmed.items.register(new ResourceLocation(Overwhelmed.MOD_ID, "gold_bead"), () ->
-                    new Item(new Item.Properties()
-                            .stacksTo(64)
-                            .rarity(Rarity.UNCOMMON)
-                            .arch$tab(overwhelmedTab)));
-            iceCrystalShardItem = Overwhelmed.items.register(new ResourceLocation(Overwhelmed.MOD_ID, "ice_crystal_shard"), () ->
-                    new Item(new Item.Properties()
-                            .stacksTo(64)
-                            .rarity(Rarity.UNCOMMON)
-                            .arch$tab(CreativeModeTabs.INGREDIENTS)));
-            iceCubeItem = Overwhelmed.items.register(new ResourceLocation(Overwhelmed.MOD_ID, "ice_cube"), () ->
-                    new Item(new Item.Properties()
-                            .stacksTo(64)
-                            .rarity(Rarity.COMMON)
-                            .arch$tab(overwhelmedTab)));
-            pebbleItem = Overwhelmed.items.register(new ResourceLocation(Overwhelmed.MOD_ID, "pebble"), () ->
-                    new Item(new Item.Properties()
-                            .stacksTo(64)
-                            .rarity(Rarity.COMMON)
-                            .arch$tab(overwhelmedTab)));
-            testBlockItem = Overwhelmed.items.register(new ResourceLocation(Overwhelmed.MOD_ID, "test_block"),
-                    () -> new BlockItem(BlockRegistry.testBlock.get(), new Item.Properties().arch$tab(overwhelmedTab)));
+        goldBeadItem = registerItem("gold_bead", 64, Rarity.UNCOMMON);
+        iceCrystalShardItem = registerItem("ice_crystal_shard", 64, Rarity.UNCOMMON);
+        iceCubeItem = registerItem("ice_cube", 64, Rarity.COMMON);
+        pebbleItem = registerItem("pebble", 64, Rarity.COMMON);
          */
 
+
         Overwhelmed.ITEMS.register();
+    }
+    private static RegistrySupplier<Item> registerItem(String name, int stackSize, Rarity rarity) {
+        return Overwhelmed.ITEMS.register(new ResourceLocation(Overwhelmed.MOD_ID, name), () ->
+                new Item(new Item.Properties()
+                        .stacksTo(stackSize)
+                        .rarity(rarity)
+                        .arch$tab(overwhelmedTab)));
     }
 }
