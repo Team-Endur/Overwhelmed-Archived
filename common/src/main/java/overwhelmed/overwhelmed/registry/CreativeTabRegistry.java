0911c26044071e4e255f9f8e0ae50c9065f66b83
Jeremiah Wimbrough <j.wimbrough@gmail.com>
2023-06-25 00:06:56 -0400

    Temporary CreativeTab fix until Registry Suppliers can be used

--- common/src/main/java/overwhelmed/overwhelmed/registry/CreativeTabRegistry.java
+++ common/src/main/java/overwhelmed/overwhelmed/registry/CreativeTabRegistry.java
@@ -1,6 +1,8 @@
 package overwhelmed.overwhelmed.registry;
 
+import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
@@ -8,9 +10,22 @@ import net.minecraft.world.item.ItemStack;
 import overwhelmed.overwhelmed.Overwhelmed;
 
 public class CreativeTabRegistry {
+    /*
     public static RegistrySupplier<CreativeModeTab> overwhelmedTab;
+     */
+    public  static final DeferredRegister<CreativeModeTab> overwhelmedCreativeTabs =
+            DeferredRegister.create(Overwhelmed.MOD_ID, Registries.CREATIVE_MODE_TAB);
+
+    public static final RegistrySupplier<CreativeModeTab> overwhelmedTab = overwhelmedCreativeTabs.register(
+            "overwhelmed_tab", () ->  dev.architectury.registry.CreativeTabRegistry.create(
+                    Component.translatable("category.overwhelmed"),
+                    () -> new ItemStack(ItemRegistry.snailShellItem.get())
+            )
+    );
 
     public static void registerCreativeTab(){
+                overwhelmedCreativeTabs.register();
+        /*
         overwhelmedTab = Overwhelmed.creativeModeTabs.register(
                 new ResourceLocation(Overwhelmed.MOD_ID, "overwhelmed_tab"),
                 () -> dev.architectury.registry.CreativeTabRegistry.create(
@@ -18,5 +33,6 @@ public class CreativeTabRegistry {
                         () -> new ItemStack(ItemRegistry.snailShellItem.get())
                 )
         );
+         */
     };
 }
