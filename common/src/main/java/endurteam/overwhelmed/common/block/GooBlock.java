5a6724f7376d9854180bef426b271db6e0475b89
Anthony Michalek (Codetoil) <ianthisawesomee@gmail.com>
2023-07-15 07:45:49 -0400

    Goo Changes

--- common/src/main/java/endurteam/overwhelmed/common/block/GooBlock.java
+++ common/src/main/java/endurteam/overwhelmed/common/block/GooBlock.java
@@ -2,14 +2,9 @@ package endurteam.overwhelmed.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.NotNull;
 
 public class GooBlock extends Block {
 
