81988c21c50ec640408becc0c9544e18f17241f8
cheesenull-butter <buttercheesenull@gmail.com>
2023-07-16 15:12:05 +0700

    New Butterfly Spawn Egg color

--- common/src/main/java/endurteam/overwhelmed/client/color/items/ButterflySpawnEggItemColor.java
+++ common/src/main/java/endurteam/overwhelmed/client/color/items/ButterflySpawnEggItemColor.java
@@ -4,8 +4,8 @@ import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.world.item.ItemStack;
 
 public class ButterflySpawnEggItemColor implements ItemColor {
-    public final int BACKGROUND_COLOR = 0xcfaf6c;
-    public final int HIGHLIGHT_COLOR = 0x534834;
+    public final int BACKGROUND_COLOR = 0x1c202c;
+    public final int HIGHLIGHT_COLOR = 0x2b56d2;
     @Override
     public int getColor(ItemStack itemStack, int i) {
         return i == 0 ? this.BACKGROUND_COLOR : this.HIGHLIGHT_COLOR;
