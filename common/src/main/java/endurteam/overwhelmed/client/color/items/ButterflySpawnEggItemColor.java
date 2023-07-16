90d189eea323edf4ac9b624d659e6f5a32a84274
Anthony Michalek (Codetoil) <ianthisawesomee@gamil.com>
2023-07-16 04:06:55 -0400

    Butterfly stuff

--- common/src/main/java/endurteam/overwhelmed/client/color/items/ButterflySpawnEggItemColor.java
+++ common/src/main/java/endurteam/overwhelmed/client/color/items/ButterflySpawnEggItemColor.java
@@ -4,8 +4,8 @@ import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.world.item.ItemStack;
 
 public class ButterflySpawnEggItemColor implements ItemColor {
-    public final int BACKGROUND_COLOR = 0xffffff;
-    public final int HIGHLIGHT_COLOR = 0x000000;
+    public final int BACKGROUND_COLOR = 0xcfaf6c;
+    public final int HIGHLIGHT_COLOR = 0x534834;
     @Override
     public int getColor(ItemStack itemStack, int i) {
         return i == 0 ? this.BACKGROUND_COLOR : this.HIGHLIGHT_COLOR;
