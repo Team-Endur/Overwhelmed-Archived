946ec8c95b4578943dc69763919b88b8973cf853
Anthony Michalek (Codetoil) <ianthisawesomee@gamil.com>
2023-07-23 05:09:27 -0400

    You can now eat Goo Soup

---
+++ common/src/main/java/endurteam/overwhelmed/world/food/OverwhelmedFoods.java
@@ -0,0 +1,8 @@
+package endurteam.overwhelmed.world.food;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class OverwhelmedFoods {
+    public static final FoodProperties GOO_SOUP =
+            new FoodProperties.Builder().nutrition(4).saturationMod(5.0f).build();
+}
