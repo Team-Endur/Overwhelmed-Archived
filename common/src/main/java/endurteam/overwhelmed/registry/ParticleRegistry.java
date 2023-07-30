4cd05efea396df7b4c5a1f428543c8f2b82ecd6a
cheesenull-butter <buttercheesenull@gmail.com>
2023-07-30 11:12:34 +0700

    Half way there!

---
+++ common/src/main/java/endurteam/overwhelmed/registry/ParticleRegistry.java
@@ -0,0 +1,12 @@
+package endurteam.overwhelmed.registry;
+
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.resources.ResourceLocation;
+import endurteam.overwhelmed.Overwhelmed;
+
+import javax.xml.namespace.QName;
+import java.util.function.Supplier;
+
+public class ParticleRegistry {
+    public static final
+}
