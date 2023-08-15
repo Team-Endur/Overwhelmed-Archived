829ce9ba5a866e3fa4ae1a09c838695c76476965
cheesenull-butter <buttercheesenull@gmail.com>
2023-08-15 18:23:10 +0700

    Leaving some sounds

--- common/src/main/java/endurteam/overwhelmed/registry/SoundEventRegistry.java
+++ common/src/main/java/endurteam/overwhelmed/registry/SoundEventRegistry.java
@@ -8,16 +8,36 @@ import net.minecraft.sounds.SoundEvent;
 public class SoundEventRegistry {
     public static RegistrySupplier<SoundEvent> snailHurt;
     public static RegistrySupplier<SoundEvent> snailDeath;
+    public static RegistrySupplier<SoundEvent> soilBreak;
+    public static RegistrySupplier<SoundEvent> soilHit;
+    public static RegistrySupplier<SoundEvent> soilPlace;
+    public static RegistrySupplier<SoundEvent> soilStep;
 
     public static void registerSoundEvents() {
         snailHurt = Overwhelmed.SOUND_EVENTS.register(
                 new ResourceLocation(Overwhelmed.MOD_ID, "entity.snail.hurt"), () ->
-                SoundEvent.createVariableRangeEvent(
-                        new ResourceLocation(Overwhelmed.MOD_ID, "entity.snail.hurt")));
+                        SoundEvent.createVariableRangeEvent(
+                                new ResourceLocation(Overwhelmed.MOD_ID, "entity.snail.hurt")));
         snailDeath = Overwhelmed.SOUND_EVENTS.register(
                 new ResourceLocation(Overwhelmed.MOD_ID, "entity.snail.death"), () ->
                         SoundEvent.createVariableRangeEvent(
                                 new ResourceLocation(Overwhelmed.MOD_ID, "entity.snail.death")));
+        soilBreak = Overwhelmed.SOUND_EVENTS.register(
+                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.break"), () ->
+                        SoundEvent.createVariableRangeEvent(
+                                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.break")));
+        soilHit = Overwhelmed.SOUND_EVENTS.register(
+                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.hit"), () ->
+                        SoundEvent.createVariableRangeEvent(
+                                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.hit")));
+        soilPlace = Overwhelmed.SOUND_EVENTS.register(
+                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.place"), () ->
+                        SoundEvent.createVariableRangeEvent(
+                                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.place")));
+        soilStep = Overwhelmed.SOUND_EVENTS.register(
+                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.step"), () ->
+                        SoundEvent.createVariableRangeEvent(
+                                new ResourceLocation(Overwhelmed.MOD_ID, "block.soil.step")));
 
         Overwhelmed.SOUND_EVENTS.register();
     }
