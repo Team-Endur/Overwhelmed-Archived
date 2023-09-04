5abec2881b180eafefc2281bf3756e0919d4d06f
cheesenull-butter <buttercheesenull@gmail.com>
2023-09-04 08:32:45 +0700

    Settled up

--- common/src/main/java/endurteam/overwhelmed/world/level/block/GooBlock.java
+++ common/src/main/java/endurteam/overwhelmed/world/level/block/GooBlock.java
@@ -1,7 +1,12 @@
 package endurteam.overwhelmed.world.level.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.HalfTransparentBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,8 +17,24 @@ public class GooBlock extends HalfTransparentBlock {
     public GooBlock(BlockBehaviour.Properties properties) {
         super(properties);
     }
+    protected static final VoxelShape SHAPE = Block.box(1.0, 0.0, 1.0, 14.0, 14.0, 14.0);
     @Override
-    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return box(0, 0, 0, 16, 14, 16);
+    public VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE;
+    }
+
+    public static void showJumpParticles(Entity entity) {
+        showParticles(entity, 10);
+    }
+
+    private static void showParticles(Entity entity, int i) {
+        if (entity.level().isClientSide) {
+            BlockState blockState = Blocks.HONEY_BLOCK.defaultBlockState();
+
+            for(int j = 0; j < i; ++j) {
+                entity.level().addParticle(new BlockParticleOption(ParticleTypes.BLOCK, blockState), entity.getX(), entity.getY(), entity.getZ(), 0.0, 0.0, 0.0);
+            }
+
+        }
     }
 }
