df5e67a0814c362bfab30f9c6fa30b56c26fe122
Anthony (Codetoil) <ianthisawesomee@gmail.com>
2024-01-21 08:52:40 -0500

    Attempt to fix Moth.java

--- common/src/main/java/endurteam/overwhelmed/world/entity/animal/Moth.java
+++ common/src/main/java/endurteam/overwhelmed/world/entity/animal/Moth.java
@@ -42,7 +42,7 @@ import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class Moth extends Monster {
+public class Moth extends PathfinderMob {
     public AnimationState flyAnimationState = new AnimationState();
     @Nullable
     private BlockPos targetPosition;
