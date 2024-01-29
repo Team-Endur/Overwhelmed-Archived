e31f574e0f7603e1e6cde93710147e1e7deb3327
cheesenull-butter <buttercheesenull@gmail.com>
2024-01-29 19:44:20 +0700

    Moth Animation

---
+++ common/src/main/java/endurteam/overwhelmed/client/animation/definitions/HornetAnimation.java
@@ -0,0 +1,63 @@
+/**
+ *  Overwhelmed, a Minecraft overhauling and adding new features to the Overworld's surface!<br>
+ *  Copyright (C) 2023-2024 Endurteam<br>
+ *  <br>
+ *  This program is free software: you can redistribute it and/or modify<br>
+ *  it under the terms of the GNU General Public License as published by<br>
+ *  the Free Software Foundation, either version 3 of the License, or<br>
+ *  any later version.<br>
+ *  <br>
+ *  This program is distributed in the hope that it will be useful,<br>
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of<br>
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br>
+ *  GNU General Public License for more details.<br>
+ *  <br>
+ *  You should have received a copy of the GNU General Public License with<br>
+ *  the Minecraft Linking Exception<br>
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/> and<br>
+ *  <https://gist.github.com/triphora/588f353802a3b0ea649e4fc85f75e583/>
+ */
+
+package endurteam.overwhelmed.client.animation.definitions;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.animation.KeyframeAnimations;
+
+import javax.naming.NameNotFoundException;
+
+@Environment(EnvType.CLIENT)
+public class HornetAnimation {
+    public static final AnimationDefinition HORNET_FLY = AnimationDefinition.Builder.withLength(4f).looping()
+            .addAnimation("body",
+                    new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                            new Keyframe(0f, KeyframeAnimations.degreeVec(7.5f, 0f, 0f),
+                                    AnimationChannel.Interpolations.CATMULLROM),
+                            new Keyframe(1f, KeyframeAnimations.degreeVec(0f, 0f, 0f),
+                                    AnimationChannel.Interpolations.CATMULLROM),
+                            new Keyframe(3f, KeyframeAnimations.degreeVec(15f, 0f, 0f),
+                                    AnimationChannel.Interpolations.CATMULLROM),
+                            new Keyframe(4f, KeyframeAnimations.degreeVec(7.5f, 0f, 0f),
+                                    AnimationChannel.Interpolations.CATMULLROM)))
+            .addAnimation("antenna",
+                    new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                            new Keyframe(0f, KeyframeAnimations.degreeVec(10f, 0f, 10f),
+                                    AnimationChannel.Interpolations.CATMULLROM),
+                            new Keyframe(2f, KeyframeAnimations.degreeVec(-10f, 0f, -10f),
+                                    AnimationChannel.Interpolations.CATMULLROM),
+                            new Keyframe(4f, KeyframeAnimations.degreeVec(10f, 0f, 10f),
+                                    AnimationChannel.Interpolations.CATMULLROM)))
+            .addAnimation("left_wing",
+                    new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                            new Keyframe(1.2916767f, KeyframeAnimations.degreeVec(0f, 0f, NaNf),
+                                    AnimationChannel.Interpolations.CATMULLROM)))
+            .addAnimation("right_wing",
+                    new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                            new Keyframe(0f, KeyframeAnimations.degreeVec(0f, 0f, NaNf),
+                                    AnimationChannel.Interpolations.CATMULLROM))).build();
+
+    private HornetAnimation() {}
+}
