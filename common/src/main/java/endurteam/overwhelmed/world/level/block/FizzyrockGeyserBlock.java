7f767040b768b615bb4052ff679b2277698a93e4
cheesenull-butter <buttercheesenull@gmail.com>
2024-02-10 16:16:36 +0700

    The particle is now spawning(No Fizzyrock for Forge and NeoForge yet)

--- common/src/main/java/endurteam/overwhelmed/world/level/block/FizzyrockGeyserBlock.java
+++ common/src/main/java/endurteam/overwhelmed/world/level/block/FizzyrockGeyserBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
@@ -18,25 +19,24 @@ public class FizzyrockGeyserBlock extends Block {
         super(properties);
     }
 
-    public void tick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-        super.animateTick(state, level, pos, random);
+    public void animateTick(BlockState blockState, Level level, BlockPos blockPos, RandomSource randomSource) {
+        VoxelShape voxelShape = this.getShape(blockState, level, blockPos, CollisionContext.empty());
+        Vec3 vec3 = voxelShape.bounds().getCenter();
+        double d = (double)blockPos.getX() + vec3.x;
+        double e = (double)blockPos.getZ() + vec3.z;
 
-        if (level.isEmptyBlock(pos.above())) {
-            double posX = pos.getX() + 0.5;
-            double posY = pos.getY() + 1.0;
-            double posZ = pos.getZ() + 0.5;
-
-            double offsetX = random.nextGaussian() * 0.02;
-            double offsetY = random.nextGaussian() * 0.02;
-            double offsetZ = random.nextGaussian() * 0.02;
-
-            level.addParticle(ParticleTypes.BUBBLE_COLUMN_UP, posX + offsetX, posY + offsetY, posZ + offsetZ, 0.0, 0.0, 0.0);
+        for(int i = 0; i < 2; ++i) {
+            if (randomSource.nextBoolean()) {
+                double yOffset = 18.0 / 18.0;
+                level.addParticle(ParticleTypes.BUBBLE_POP, d + randomSource.nextDouble() / 5.0, (double)blockPos.getY() + yOffset + (0.5 - randomSource.nextDouble()), e + randomSource.nextDouble() / 5.0, 0.0, 0.0, 0.0);
+            }
         }
     }
 
+
     @Override
     public @NotNull VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return box(4.0f, 0.0f, 4.0f, 4.0f, 0.0f, 4.0f);
+        return box(4.0f, 0.0f, 4.0f, 12.0f, 16.0f, 12.0f);
     }
 
 }
